7788401291264c76954a4ee0d075c147617b4557
binaryta <naritatakuya0000@gmail.com>
2025-04-03 23:45:04 +0900

    Replace github/copilot.vim with zbirenbaum/copilot.lua

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,5 +1,22 @@
 return {
-  "github/copilot.vim",
+  "zbirenbaum/copilot.lua",
   cmd = "Copilot",
-  lazy = false,
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = true,
+        debounce = 75,
+        keymap = {
+          accept = "<Tab>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-s>",
+          prev = "<C-p>",
+          dismiss = "<C-]>",
+        },
+      },
+    })
+  end,
 }
